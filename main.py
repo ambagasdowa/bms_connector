aaaf43aecd07f44b5d51645a6a905e7b19d52ede
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-11-09 10:25:16 -0600

    Trying crud modeler v_0.0.1

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 
 
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
+#from sqlalchemy.orm import Session
 
 #from . import crud, models, schemas
 #from .database import SessionLocal, engine
@@ -30,12 +30,12 @@ bms = FastAPI(
 
 
 # Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+#def get_db():
+#    db = SessionLocal()
+#    try:
+#        yield db
+#    finally:
+#        db.close()
 
 
 @bms.get('/')
