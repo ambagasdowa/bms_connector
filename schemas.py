6b72da4d485fd1cb8f00c418ce351a4465e8b005
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-11-11 17:51:11 -0600

    Building an api step 0.0.30

--- schemas.py
+++ schemas.py
@@ -56,7 +56,7 @@ class PageBase(BaseModel):
 #    is_url: bool
 #    book_pages: int
     path: str
-    css : Optional[Dict]
+    css : Optional[str] = None
 
 class PageCreate(BaseModel):
     pass
