9bbf5b5c5f37fbaa1dd3a684fa85d71c66a78154
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-11-11 20:06:01 -0600

    Testing joins stp 0.0.21

--- models.py
+++ models.py
@@ -36,7 +36,7 @@ class Page(Base):
     __tablename__ = "bms_view_inputs"
 
     id = Column(String, primary_key=True, index=True)
-    book_id = Column(String,  ForeignKey("bms_books.book_id"))
+    book_id = Column(String,  ForeignKey("bms_cache_books.book_id"))
     book_name = Column(String, index=True)
     is_url = Column(Boolean, default=False)
     book_pages = Column(Integer)
