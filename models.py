20bac2a2cffaaf37c1e93388fddac06bf3efb66a
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-11-12 18:22:03 -0600

    Upgrade models.py

--- models.py
+++ models.py
@@ -44,7 +44,6 @@ class Item(Base):
 #        "Input", foreign_keys=[users_id])
 
 
-
 class Page(Base):
     __tablename__ = "bms_view_inputs"
 
@@ -60,10 +59,10 @@ class Page(Base):
 
 
 class Input(Base):
-    __tablename__ = "bms_cache_view_users_inputs"
+    __tablename__ = "bms_view_users_inputs"
 
-    id = Column(String, primary_key=True, index=True)
-    input_id = Column(Integer)
+#    id = Column(String, primary_key=True, index=True)
+    input_id = Column(Integer, primary_key=True, index=True)
     book_id = Column(String, ForeignKey("bms_cache_books.book_id"))
     bms_bookpages_id = Column(String)
     label = Column(String, index=True)
