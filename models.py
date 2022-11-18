389762a438805034e3733a61cdf824c1f83e278f
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-11-18 09:10:53 -0600

    Working in api models and schemas v_0.0.10

--- models.py
+++ models.py
@@ -39,14 +39,13 @@ class Item(Base):
                              primaryjoin="and_(Item.book_id==Position.bms_books_id,Page.id==Position.bms_bookpages_id)"
                              )
     inputs = relationship("Input",
-#                          secondary="outerjoin(Page,Inpage,Input.id==Inpage.bms_inputs_ctrls_id)",
+                          secondary="outerjoin(Input,Inpage,Input.id==Inpage.bms_inputs_ctrls_id)",
                           primaryjoin="and_(Item.book_id==Input.bms_books_id,Page.id==Position.bms_bookpages_id)"
                           )
-    inpages = relationship("Inpage",
-                            primaryjoin="and_(Input.id==Inpage.bms_inputs_ctrls_id)",
-                           viewonly=True
-                          )
-
+#    inpages = relationship("Inpage",
+#                            primaryjoin="and_(Input.id==Inpage.bms_inputs_ctrls_id)"
+#                          )
+#
 #
 
 
