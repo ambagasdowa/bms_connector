047c8194f1b432000037fbee4e4e5a3a7a087750
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-11-18 09:01:08 -0600

    Working in api models and schemas v_0.0.7

--- schemas.py
+++ schemas.py
@@ -145,6 +145,7 @@ class Item(ItemBase):
     pagination: Union[List[Page]] = []
     positions: Union[List[Position]] = []
     inputs: Union[List[Input]] = []
+    inpages: Union[List[Inpage]] = []
 #    inpages: Optional[str] = []
 #    values: Union[List[Value]] = []
 #    values: Union[List[Value]] = []
