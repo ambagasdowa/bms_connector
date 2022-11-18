e175bc079c064bc1c23e1315bacacbfac72b28bb
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-11-18 11:07:09 -0600

    Working in api models and schemas v_0.0.11

--- models.py
+++ models.py
@@ -43,7 +43,7 @@ class Item(Base):
                           primaryjoin="and_(Item.book_id==Input.bms_books_id,Page.id==Position.bms_bookpages_id)"
                           )
     inpages = relationship("Inpage",
-                            join="outerjoin(Input,Inpage,Input.id==Inpage.bms_inputs_ctrls_id)"
+                            secondary="outerjoin(Input,Inpage,Input.id==Inpage.bms_inputs_ctrls_id)"
                           )
 #
 #
