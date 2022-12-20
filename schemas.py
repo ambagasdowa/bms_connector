d366f2655b96d663787bfb2ed0bc116a45d64492
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-20 16:33:13 -0600

    reorder schemas v 0.0.4

--- schemas.py
+++ schemas.py
@@ -133,6 +133,19 @@ class ItemBase(BaseModel):
     is_url: bool
 #    modified: datetime
 #    status: bool
+    pagination: Union[List[Page]] = []
+    positions: Union[List[Position]] = []
+    # This goes togheter
+    inputs: Union[List[Input]] = []
+    inpages: Union[List[Inpage]] = []
+    invalues: Union[List[Invalue]] = []
+
+    created: datetime
+
+    def dict(self, **kwargs):
+        data = super(ItemBase, self).dict(**kwargs)
+        data['xpaper'] = [data['inpages'], data['invalues']]
+        return data
 
 
 class ItemCreate(BaseModel):
@@ -155,19 +168,6 @@ class ItemUpdate(BaseModel):
 
 class Item(ItemBase):
     id: int
-    pagination: Union[List[Page]] = []
-    positions: Union[List[Position]] = []
-    # This goes togheter
-    inputs: Union[List[Input]] = []
-    inpages: Union[List[Inpage]] = []
-    invalues: Union[List[Invalue]] = []
-
-    created: datetime
-
-    def dict(self, **kwargs):
-        data = super(Item, self).dict(**kwargs)
-        data['xpaper'] = [data['inpages'], data['invalues']]
-        return data
 
     class Config:
         orm_mode = True
