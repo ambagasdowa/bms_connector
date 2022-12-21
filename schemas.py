5d8f0771019bb3e88c9eaddab9512de94581bec6
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-21 10:08:58 -0600

    working in schemas.py

--- schemas.py
+++ schemas.py
@@ -179,6 +179,8 @@ class ItemBase(BaseModel):
         book_inputs = {}
         ins = {}
         for input_pages in data['inputs']:
+            if book_inputs[input_pages['bms_bookpages_id']] is None:
+                book_inputs[input_pages['bms_bookpages_id']] = {} 
             print(input_pages['bms_bookpages_id'])
             for attr in input_pages['data']:
                 ins[attr['attribute']] = attr['value']
