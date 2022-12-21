9dea6475ba53b4a21b30a36fe39ff70f0f6b5577
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-20 19:31:56 -0600

    reorder schemas v 0.0.28

--- schemas.py
+++ schemas.py
@@ -179,7 +179,7 @@ class ItemBase(BaseModel):
         ins = {}
         for input_pages in data['inputs']:
             for attr in input_pages['data']:
-                ins[attr['attribute']] = attr['value']
+                ins[attr['attribute']].append(attr['value'])
             book_inputs[input_pages['bms_bookpages_id']].append(ins)
 
         data['book_inputs'] = book_inputs
