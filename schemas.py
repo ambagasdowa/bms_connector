0ca7e20685e3073e99ec1b908f989fbeb3ef46dc
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-20 19:33:24 -0600

    reorder schemas v 0.0.29

--- schemas.py
+++ schemas.py
@@ -175,14 +175,14 @@ class ItemBase(BaseModel):
         del data['positions']
 
         # Rearrange inputs
-        book_inputs = {}
-        ins = {}
-        for input_pages in data['inputs']:
-            for attr in input_pages['data']:
-                ins[attr['attribute']].append(attr['value'])
-            book_inputs[input_pages['bms_bookpages_id']].append(ins)
+        # book_inputs = {}
+        # ins = {}
+        # for input_pages in data['inputs']:
+        #     for attr in input_pages['data']:
+        #         ins[attr['attribute']].append(attr['value'])
+        #     book_inputs[input_pages['bms_bookpages_id']].append(ins)
 
-        data['book_inputs'] = book_inputs
+        # data['book_inputs'] = book_inputs
         return data
 
 
