91f88955f5e37140b4642dbffe99b6f54290b803
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-21 09:09:03 -0600

    working in schemas.py

--- schemas.py
+++ schemas.py
@@ -178,14 +178,14 @@ class ItemBase(BaseModel):
         # Change the column name in sql table usr_attr and usr_value
         book_inputs = {}
         ins = {}
-        for input_pages in data['inputs']:
-            for attr in input_pages['data']:
-                if attr.get('attribute') is None:
-                    ins[attr['usr_attr']] = attr['usr_value']
-                else:
-                    ins[attr['attribute']] = attr['value']
+        # for input_pages in data['inputs']:
+        #     for attr in input_pages['data']:
+        #         if attr.get('attribute') is None:
+        #             ins[attr['usr_attr']] = attr['usr_value']
+        #         else:
+        #             ins[attr['attribute']] = attr['value']
 
-            book_inputs[input_pages['bms_bookpages_id']].append(ins)
+        #     book_inputs[input_pages['bms_bookpages_id']].append(ins)
 
         # data['book_inputs'] = book_inputs
         return data
