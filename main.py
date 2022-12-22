7c75177500662f4868389db408de779b580527f1
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-22 17:54:43 -0600

    Working in crud.py

--- main.py
+++ main.py
@@ -67,12 +67,7 @@ def items_action_retrieve(item_id: str, user_id: int, db: Session = Depends(get_
     item = crud.get_items(db, item_id, user_id)
     if item is None:
         raise HTTPException(status_code=404, detail="Book not found")
-#    json_compatible_item_data = jsonable_encoder(item)
-#    return JSONResponse(content=json_compatible_item_data)
-
-#    json_str = json.dumps(item, indent=14, default=str)
-#    return Response(content=json_str, media_type='application/json')
-     return item
+    return item
 
 # CREATE
 # This endpoint creates a new `Item`. The necessary data is read from the request
