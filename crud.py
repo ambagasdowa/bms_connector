4e496f9ed0b2360298e508570fba1f80439698ff
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-24 09:18:38 -0600

    Add features for files uploads

--- crud.py
+++ crud.py
@@ -1,5 +1,6 @@
 # crud.py
 from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
 from .models import Item, Page
 from .schemas import ItemCreate, ItemUpdate
@@ -20,7 +21,6 @@ import hashlib
 from .config import configuration
 
 config = configuration['download_config']
-
 
 
 def list_items(db: Session, skip: int = 0, limit: int = 100):
@@ -75,27 +75,27 @@ def get_pages(db: Session, book_id: str):
     return db.query(Page).filter(Page.book_id == book_id).all()
 
 # One with have the files
+
+
 def get_files(path):
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
             yield file
 
 
-
-
-def store_file(file):
+def store_file(file, token):
     """TODO: Docstring for store_file.
 
     :arg1: TODO
     :returns: TODO
     https --verify=no -f POST 10.14.17.105:8000/upload files@~/Development/book_matematicas_002_bachillerato_20221223.zip files@~/Development/guia_unam_215_universidad_20221223.zip files@~/Development/guia_uv_002_demo_20221223.zip
     """
-
+    print(f"token : {token}")
     filename = file.filename
     tmp_path = config['download_path'] + config['dir_path']
 #    clean_dir_files = subprocess.run(["rm", "-r", tmp_path], stdout=subprocess.DEVNULL)
-#book_matematicas_002_bachillerato_20221223.zip
-    spl = str(filename.replace('.zip','')).split('_')
+# book_matematicas_002_bachillerato_20221223.zip
+    spl = str(filename.replace('.zip', '')).split('_')
 
     type_book = spl[0]
     type_course = spl[1]
@@ -103,11 +103,12 @@ def store_file(file):
     type_degree = spl[3]
    # type_date = spl[4]
 
-    dir_path ='/'+type_book+'/'+type_course+'/'+type_serial+'/'+type_degree
+    dir_path = '/'+type_book+'/'+type_course+'/'+type_serial+'/'+type_degree
     #dir_path = (split_data[0]+'/'+split_data[1], split_data[0],md5_returned, datetime.now().isoformat(timespec='seconds'), '', 1,)
 
     store_path = config['basename']+config['pathname']+dir_path
-    make_dir_files = subprocess.run(["mkdir", "-p", tmp_path+"pack", tmp_path+"unpack",store_path], stdout=subprocess.DEVNULL)
+    make_dir_files = subprocess.run(
+        ["mkdir", "-p", tmp_path+"pack", tmp_path+"unpack", store_path], stdout=subprocess.DEVNULL)
 
 #    for file in files :
     print(file)
@@ -123,7 +124,7 @@ def store_file(file):
         with zipfile.ZipFile(filename, 'r') as zip_ref:
             zip_ref.extractall(store_path)
     except zipfile.BadZipfile:
-        print("[red] zip file : "+ filename +
+        print("[red] zip file : " + filename +
               " from provider with errors , try again ...[red]")
 
     xfiles = []
@@ -131,6 +132,19 @@ def store_file(file):
         xfiles.append(xfile)
 
     print(xfiles)
+
+    # Insert data
+    # with Session(bind=engine) as session:
+
+    #     book1 = Book(title="Dead People Who'd Be Influencers Today")
+    #     book2 = Book(title="How To Make Friends In Your 30s")
+
+    #     author1 = Author(name="Blu Renolds")
+    #     author2 = Author(name="Chip Egan")
+    #     author3 = Author(name="Alyssa Wyatt")
+
+    #     session.add_all([book1, book2, author1, author2, author3])
+    #     session.commit()
 
     # # Open,close, read file and calculate MD5 on its contents
     #     with open(source, 'rb') as file_to_check:
@@ -171,5 +185,5 @@ def store_file(file):
     #         cursor.commit()
     #         files_ids.append(str(cmex_api_controls_files_id))
 
-    #return {"success": f"Now going to process your files {[file.filename for file in files]}"}
-    return {"success": f"Now going to process your files {filename}"}
+    # return {"success": f"Now going to process your files {[file.filename for file in files]}"}
+    return {"success": f"Now going to process your files {filename} whit token : {token}"}
