0a9b3a9160f50c37bc7006603d30d6cbc6f049fe
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-26 17:29:59 -0600

    Update work on crud advance

--- schemas.py
+++ schemas.py
@@ -244,12 +244,11 @@ class FileCreate(BaseModel):
              "example" : {
                   "book_id": 10,
                   "pages": 219,
-                  "book_name": "Guia Estudio Matematicas",
+                  "book_name": "Guia de Estudio de Matematicas",
                   "is_url": true,
                   "created": "2022-12-26T23:23:02.071Z",
                   "modified": "2022-12-26T23:23:02.071Z",
                   "status": true,
-                  "id": 2
             }
          }
 
