379fee4fe7e4f6c4328141712498d48072d172ca
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-26 18:51:07 -0600

    Update work on crud advance

--- crud.py
+++ crud.py
@@ -135,6 +135,7 @@ def store_file(db:Session , token:str, file):
         "_filename":filename
         ,"_pathname":store_path
     }
+    print(type(book))
     db_file = File(**book.dict())
     session.add(db_file)
     session.commit()
