db5cc44c8769ea4d243a8b2681992cdaf41aad1b
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-28 13:45:27 -0600

    Add Parameter book_name in post(upload)

--- crud.py
+++ crud.py
@@ -115,8 +115,8 @@ def store_file(db:Session, book_name:str , token:str, file):
     if book_name : 
         print(f"TYPE of Book : {type(book_name)}")
         print(book_name)
-        for key,value in book_name.items():
-            print(key,value)
+        for index,value in book_name:
+            print(index,value)
 
     if not book_name :
         book_name = spl.replace('_',' ').capitalize()
