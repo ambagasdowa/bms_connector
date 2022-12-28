96ae6b2b489a5eb80d47124c89dfb76f79a02a6e
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2022-12-28 16:30:07 -0600

    Add Parameter book_name in post(upload)

--- schemas.py
+++ schemas.py
@@ -191,7 +191,7 @@ class ItemBase(BaseModel):
             book_inputs[page].append(ins[page])
 
         data['book_inputs'] = book_inputs
-        del data['inputs']
+#        del data['inputs']
         del data['inpages']
         del data['positions']
         del data['invalues']
