b3d2d33e0b9c885bc7f3d7d9ffe9171b0a8797d0
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-01-11 19:44:21 -0600

    Adding Method bms_src_positions

--- main.py
+++ main.py
@@ -254,7 +254,7 @@ def files_action_retrieve(file_id: int, data: schemas.FileUpdate,  db: Session =
 
 @app.get("/srcpos/{book_id}/{page_id}", response_model=List[schemas.SourcePositions])
 def srcpos_action_retrieve(book_id: int,page_id: int, db: Session = Depends(get_db)):
-   srcpos = crud.get_srcpos(db, item_id)
+   srcpos = crud.get_srcpos(db, book_id, page_id)
    if srcpos is None:
        raise HTTPException(status_code=404, detail="SourcePositions not found")
    return srcpos
