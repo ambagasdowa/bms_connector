024a1988f3bd0de7668e8c1310e7782631fcbb2b
Legion <baizabal.jesus@gmail.com>
2023-02-07 21:25:05 -0600

    Change in crud

--- models.py
+++ models.py
@@ -80,6 +80,7 @@ class Upload(Base):
 
 class File(Base):
     __tablename__ = "bms_books"
+
     id = Column(Integer, primary_key=True, index=True)
     book_id = Column(Integer)
     pages = Column(Integer)
@@ -111,7 +112,6 @@ class File(Base):
 
 class Item(Base):
     __tablename__ = "bms_books"
-    # __table_args__ = {'extend_existing': True}
 
     id = Column(Integer, primary_key=True, index=True)
     book_id = Column(Integer, index=True)
