407dda4daf754c6ea09b9828a46dbc5ea7727531
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-09 12:26:09 -0600

    Improvements in Book Detail Response of books

--- crud.py
+++ crud.py
@@ -380,10 +380,10 @@ def get_book_usr(db:Session,book_id:int,user_id:int):
     # Build the object 
     # Do the merge whit the response
     response = db.query(File).filter(File.book_id == book_id).all()
-
-    for key, value in response:
+    print(response.keys())
+    resp_data = response.dict(exclude_unset=True)
+    for key, value in hero_data.items():
         print( key, value)
-
 
 
 
