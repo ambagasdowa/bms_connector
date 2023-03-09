4eaddb5e7f8325141e1fae65ee4a88bf6aa98ae2
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-09 09:41:10 -0600

    Improvements in Book Detail Response of books

--- crud.py
+++ crud.py
@@ -381,8 +381,8 @@ def get_book_usr(db:Session,book_id:int,user_id:int):
     # Do the merge whit the response
     response = db.query(File).filter(File.book_id == book_id).all()
     print(type(response))
-    for k,v in response.items():
-        print(k,v)
+    for idx in response:
+        print(idx)
     #response["book_inputs"] = book_inputs
     if len(response) == 0 :
         print(f"[red]book id[/red] [cyan] {book_id}[/cyan][red] not found")
