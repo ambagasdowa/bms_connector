57ea711634efe1ad80cd64e7e45d90ae549ceba2
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-09 14:27:50 -0600

    Improvements in Book Detail Response of books

--- crud.py
+++ crud.py
@@ -387,8 +387,8 @@ def get_book_usr(db:Session,book_id:int,user_id:int):
     book_inputs=('test','key',) 
 
 
-    setattr(response, "book_inputs", book_inputs)
-
+    # setattr(response, "book_inputs", book_inputs)
+    response.append(book_inputs)
     # response = {**xresponse,"books_inputs":book_inputs}
 
 
