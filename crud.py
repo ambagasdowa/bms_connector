26953ec5a2da77c0657b505fb8c465fe3ec02bce
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-09 16:29:52 -0600

    Improvements in Book Detail Response of books

--- crud.py
+++ crud.py
@@ -396,6 +396,7 @@ def get_book_usr(db:Session,book_id:int,user_id:int):
         print(z.sourcePositions)
         print(z.book_name)
         z.book_name = "this is a name test ...."
+        print(dir(z.book_inputs))
         z['book_inputs'] = book_inputs
         for inp in z.book_inputs:
             print(dir(inp))
