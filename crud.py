76a69716ce9d309c706a0da73ea00cb81c9818ea
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-09 10:03:00 -0600

    Improvements in Book Detail Response of books

--- crud.py
+++ crud.py
@@ -405,7 +405,7 @@ def get_book_usr(db:Session,book_id:int,user_id:int):
 
     
     for i in range(len(response)):
-        print(dict(response))
+        print(dict([response]))
 
     #response["book_inputs"] = book_inputs
     if len(response) == 0 :
