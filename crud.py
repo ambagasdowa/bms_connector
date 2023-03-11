425b388e9c0d8b4ccc28dd1214faf2793f46263c
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-11 15:37:22 -0600

    Improvements in Book Detail Response of books

--- crud.py
+++ crud.py
@@ -396,7 +396,7 @@ def get_book_usr(db:Session,book_id:int,user_id:int):
     print(jsonable_encoder(book_inputs))
 
     response = db.query(File).filter(File.book_id == book_id).all()
-    # setattr(response[0], 'book_inputs', jsonable_encoder(books))
+    setattr(response[0], 'book_inputs', jsonable_encoder(books))
     print("[red]JASONABLE:")
     print(jsonable_encoder(response))
 
