ca0aa6e0f6a5f86c3ec8824619bbdc9dc96b39b7
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-12 13:32:30 -0600

    Improvements in Book Detail Response of books v0.0.2

--- crud.py
+++ crud.py
@@ -383,10 +383,10 @@ def get_book_usr(db:Session,book_id:int,user_id:int):
             print(jsonable_encoder(k))
             print(f"[green]{k.id},{k.bms_inputs_ctrls_id},{k.attribute},{k.value}[/green]")
             if book_input[b.bms_bookpages_id] is None:
-                book_input[b.bms_bookpages_id] = {}
+                book_input[b.bms_bookpages_id] = []
 
             #book_input[b.bms_bookpages_id][k.attribute] = k.value
-            book_input[b.bms_bookpages_id] = (jsonable_encoder(k),)
+            book_input[b.bms_bookpages_id].append(jsonable_encoder(k),)
 
             # books.append((b.bms_bookpages_id,k.id,k.attribute,k.value),)
 
