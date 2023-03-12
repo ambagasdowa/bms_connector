e5db49577a4e62d219d00923bd490a962ad0c3ff
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-12 15:12:28 -0600

    Improvements in Book Detail Response of books v0.0.2

--- main.py
+++ main.py
@@ -339,7 +339,7 @@ def books_action_list(limit: int = 100, offset: int = 0, db: Session = Depends(g
 
 # @app.get("/books/{book_id}/{user_id}", response_model=List[schemas.File])
 #response_model=List[Union[Specific, Simple]]
-@app.get("/books/{book_id}/{user_id}", response_model=List[Union[schemas.File]])
+@app.get("/books/{book_id}/{user_id}", response_model=List[Union[schemas.Item]])
 def books_action_retrieve(book_id: str, user_id: int, db: Session = Depends(get_db)):
     books = crud.get_book(db, book_id, user_id)
     if books is None:
