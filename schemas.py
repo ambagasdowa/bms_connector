b134ea51edb3ef4de2ab951ea8a261f3bec57539
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-16 10:55:53 -0600

    Improvements in Book Detail Response of books v0.0.6

--- schemas.py
+++ schemas.py
@@ -288,9 +288,8 @@ class ItemBase(BaseModel):
         book_pages_maps = {}
         for bookpagesmaps in data['positions']:
             if book_pages_maps.get(bookpagesmaps['page']) is None:
-                book_pages_maps[bookpagesmaps['page']] = ['']
-            book_pages_maps[bookpagesmaps['page']
-                            ] += f"{bookpagesmaps['css']}"
+                book_pages_maps[bookpagesmaps['page']] = ''
+            book_pages_maps[bookpagesmaps['page']] += f"{bookpagesmaps['css']}"
 
         data['book_pages_maps'] = book_pages_maps
 
